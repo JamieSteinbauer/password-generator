255c1b2920b4f2f643f2255d0b88133ad4ef4503
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-05-25 15:21:35 -0400

    fixed allowed number of characters

--- Develop/script.js
+++ Develop/script.js
@@ -25,7 +25,7 @@ function generatePassword() {
   var results = "";
   var passwordLength = prompt("How long would you like your password to be? (8-128 characters)");
   var charQty = parseInt(passwordLength);
-  if (charQty > 8 || charQty < 128) {
+  if (charQty > 7 || charQty < 129) {
     var lowerCase = window.confirm("Do you want lowercase letters in your password?");
     var upperCase = window.confirm("Do you want uppercase letters in your password?");
     var numeric = window.confirm("Do you want numbers in your password?");
@@ -60,30 +60,30 @@ function generatePassword() {
   }
 }
 
-function chooseCharacters() {
-  var characterTypesArr = [];
-  var characterTypes = prompt("What character types would you like in your password? (lowercase, uppercase, numeric, special)");
-  if (characterTypes === "lowercase") {
-    var lowercase = window.confirm("abcdefghijklmnopqrstuvwxyz");
-    return lowercase;
-  }
-  else if (characterTypes === "uppercase") {
-    var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    return uppercase;
-  }
-  else if (characterTypes === "numeric") {
-    var numeric = "0123456789";
-    return numeric;
-  }
-  else if (characterTypes === "special") {
-    var special = "!@#$%^&*()_+";
-    return special;
-  }
-  else {
-    alert("You must choose at least one character type");
-    return generatePassword();
-  }
-}
+// function chooseCharacters() {
+//   var characterTypesArr = [];
+//   var characterTypes = prompt("What character types would you like in your password? (lowercase, uppercase, numeric, special)");
+//   if (characterTypes === "lowercase") {
+//     var lowercase = window.confirm("abcdefghijklmnopqrstuvwxyz");
+//     return lowercase;
+//   }
+//   else if (characterTypes === "uppercase") {
+//     var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     return uppercase;
+//   }
+//   else if (characterTypes === "numeric") {
+//     var numeric = "0123456789";
+//     return numeric;
+//   }
+//   else if (characterTypes === "special") {
+//     var special = "!@#$%^&*()_+";
+//     return special;
+//   }
+//   else {
+//     alert("You must choose at least one character type");
+//     return generatePassword();
+//   }
+// }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
