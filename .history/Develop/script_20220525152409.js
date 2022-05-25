5bf535727a3d83b960e548ac11645aff115c2266
JamieSteinbauer <jsteinbauer14@gmail.com>
2022-05-25 15:34:09 -0400

    created README.md

---
+++ .history/Develop/script_20220525152409.js
@@ -0,0 +1,65 @@
+var generateBtn = document.querySelector("#generate");
+
+const myArrayUpper = Array.from(Array(26)).map((e,i) => i + 65);  
+const alphabetUpper = myArrayUpper.map((x) => String.fromCharCode(x));
+
+const myArrayLower = Array.from(Array(26)).map((e,i) => i + 97);
+const alphabetLower = myArrayLower.map((x) => String.fromCharCode(x));
+
+const myArrayNumeric = Array.from(Array(10)).map((e,i) => i + 48);
+const numericArray = myArrayNumeric.map((x) => String.fromCharCode(x));
+
+const myArraySpecial = Array.from(Array(15)).map((e,i) => i + 33);
+const specialArray = myArraySpecial.map((x) => String.fromCharCode(x));
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+function generatePassword() {
+  var results = "";
+  var passwordLength = prompt("How long would you like your password to be? (8-128 characters)");
+  var charQty = parseInt(passwordLength);
+  if (charQty > 7 || charQty < 129) {
+    var lowerCase = window.confirm("Do you want lowercase letters in your password?");
+    var upperCase = window.confirm("Do you want uppercase letters in your password?");
+    var numeric = window.confirm("Do you want numbers in your password?");
+    var special = window.confirm("Do you want special characters in your password?");
+  } else {
+    window.alert("Invalid entry. Select a length between 8 and 128");
+    return generatePassword;
+  }
+
+  var arrayOfCharacters = [];
+
+  if(lowerCase === true) {
+    arrayOfCharacters.push(...alphabetLower);
+  } 
+  if(upperCase === true) {
+    arrayOfCharacters.push(...alphabetUpper);
+  }
+  if(numeric === true) {
+    arrayOfCharacters.push(...numericArray);
+  }
+  if(special === true) {
+    arrayOfCharacters.push(...specialArray);
+  }
+
+  if(lowerCase || upperCase || numeric || special) {
+    for (var i = 0; i < charQty; i++) {
+      results += arrayOfCharacters[Math.floor(Math.random() * arrayOfCharacters.length)];
+    }
+  } else {
+    window.alert("A minimum of one variable must be selected to generate a password")
+    return generatePassword();
+  }
+  return results;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# password-generator
+# Password Generator
+
+# Description
+A site that prompts the user for how long they want their password and what kind of characters they want (uppercase, lowercase, special, numeric).
+
+# Website
+ https://jamiesteinbauer.github.io/password-generator/
+
+ # Screenshot
+ 
